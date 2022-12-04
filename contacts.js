566e50b122dcaae5f3f4bda9996dbc208603ce0d
Volodymyr-bart <61430064+Volodymyr-bart@users.noreply.github.com>
2022-12-04 20:39:18 +0200

    add path and fs

--- contacts.js
+++ contacts.js
@@ -1,7 +1,14 @@
-/*
- * Розкоментуйте і запиши значення
- * const contactsPath = ;
- */
+const path = require("path");
+const fs = require("fs");
+const contactsPath = path.resolve("./db/contacts.json");
+
+fs.readFile(contactsPath, "utf-8", (error, data) => {
+  if (error) {
+    console.log(error);
+  }
+  console.log(data);
+});
+// fs.writeFile()
 
 // TODO: задокументувати кожну функцію
 function listContacts() {
